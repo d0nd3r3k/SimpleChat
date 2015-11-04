3e528a8d92bb9de3b8ec011c59c74dc203eae841
Donald Derek <donaldderek.haddad@gmail.com>
2015-11-04 14:00:10 -0500

    big refactor

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var port = 3700;
+var port = 8888;
 
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
@@ -10,7 +10,6 @@ app.use(express.static(__dirname + '/public'));
 app.get("/", function(req, res){
 	res.render("page");
 });
-
 
 var io = require('socket.io').listen(app.listen(port));
 
--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 
 	var messages = [];
-	var socket = io.connect('http://localhost:3700');
+	var socket = io.connect('http://'+location.host);
 	var field = document.getElementById("field");
 	var sendButton = document.getElementById("send");
 	var content = document.getElementById("content");
